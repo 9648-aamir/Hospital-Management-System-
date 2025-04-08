2620b8a355d37e079952ea353af53fa6ad5bbc85
9648-aamir <aamir9648770208@gmail.com>
2025-04-08 16:20:04 +0530

    Initial commit

---
+++ Hospital-Managemnet-System-main/src/pages/doctor/Doctors.jsx
@@ -0,0 +1,16 @@
+import DoctorProfile from "../../components/doctorProfile/DoctorProfile"
+
+
+const Doctors = () => {
+
+  
+
+  return (
+    <div>
+      <h1>Doctore</h1>
+      <DoctorProfile/>
+    </div>
+  )
+}
+
+export default Doctors
