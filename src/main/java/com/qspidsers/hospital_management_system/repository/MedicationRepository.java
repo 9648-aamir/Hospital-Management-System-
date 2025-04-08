392e8604fffe06ded4eb74103c58e95bbfe73b7b
Aamir Shahab <91050703+9648-aamir@users.noreply.github.com>
2025-04-08 16:02:58 +0530

    Delete src directory

--- src/main/java/com/qspidsers/hospital_management_system/repository/MedicationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.qspidsers.hospital_management_system.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.qspidsers.hospital_management_system.entity.Medication;
-
-public interface MedicationRepository extends JpaRepository<Medication, Integer>{
-
-}
